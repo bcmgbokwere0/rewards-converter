d9c4761e7492ba4cb63f5c3fb427aa4fff77d7f5
bcmgbokwere0 <bcmgbokwere@gmail.com>
2023-06-20 17:40:29 -0500

    Setting up constructors and initializing instance variables.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,3 +1,20 @@
 public class RewardValue {
+        // Miles to Cash Conversion
+        final double MTC = 0.0035;
+        private double rewardCash;
+        private int rewardMiles;
+
+        public RewardValue(double cash) {
+            rewardCash = cash;
+        }
+        public RewardValue(int miles) {
+            rewardMiles = miles;
+        }
+
+
+
+
+
 
 }
+
