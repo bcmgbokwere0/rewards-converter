54df04d3695bb33ef7a4eef8c85e051827e9921d
bcmgbokwere0 <bcmgbokwere@gmail.com>
2023-06-20 18:25:24 -0500

    Standardized rewardValue to be in cash.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,26 +1,24 @@
 public class RewardValue {
         // Miles to Cash Conversion
         final double MTC = 0.0035;
-        private double rewardCash;
-        private int rewardMiles;
+        private double rewardCashValue;
 
         public RewardValue(double cash) {
-            rewardCash = cash;
+            rewardCashValue = cash;
         }
         public RewardValue(int miles) {
-            rewardMiles = miles;
+            rewardCashValue = miles * MTC;
         }
 
         public double getCashValue(){
 
-                return rewardCash;
+                return rewardCashValue;
         }
 
         public int getMilesValue(){
 
-                return rewardMiles;
+                return (int)(rewardCashValue / MTC);
         }
-
 
 }
 
